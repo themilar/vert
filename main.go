3eac10e3702e0c9b6f6b10c727563af44a13a1da
Oluwadamilare <joeydre69@gmail.com>
2023-11-21 03:20:51 +0100

    switch from if to switch with cases for other temperatures

--- main.go
+++ main.go
@@ -18,18 +18,32 @@ var description = map[string]Temperature{"Kelvin": {Value: 0, Unit: "Kelvin", Sy
 // methods
 func (t Temperature) convert() (float64, error) {
 	var k float64
-	if t.Unit != "Kevin" {
+	switch t.Unit {
+	case "Celsius":
 		k = t.Value + 273.15
-		if k < 0 {
-			return 0, errors.New("temperature can't go below absolute zero")
-		}
-		return k, nil
+	case "Farenheit":
+		k = (t.Value-32)*5/9 + 273.5
+	case "Rakine":
+		k = t.Value * 5 / 9
+	default:
+		k = t.Value
 	}
-	return t.Value, nil
+	if k < 0 {
+		return 0, errors.New("temperature can't go below absolute zero")
+	}
+	return k, nil
+	// if t.Unit != "Kevin" {
+	// 	k = t.Value + 273.15
+	// 	if k < 0 {
+	// 		return 0, errors.New("temperature can't go below absolute zero")
+	// 	}
+	// 	return k, nil
+	// }
+	// return t.Value, nil
 }
 
 func main() {
-	var boiling = Temperature{Value: 100, Unit: "celsius", Symbol: "C"}
+	var boiling = Temperature{Value: 100, Unit: "Celsius", Symbol: "C"}
 	fmt.Println(boiling)
 	fmt.Println(boiling.convert())
 	fmt.Println(description)
