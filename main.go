863706a24da0f4869164e59116fafdc95f5a33a9
Oluwadamilare <joeydre69@gmail.com>
2023-11-21 03:25:01 +0100

    starting work on other methods and testing out temperature value within kelvin method

--- main.go
+++ main.go
@@ -16,35 +16,44 @@ var description = map[string]Temperature{"Kelvin": {Value: 0, Unit: "Kelvin", Sy
 	"Rakine":    {Symbol: "Ra"}}
 
 // methods
-func (t Temperature) convert() (float64, error) {
-	var k float64
+func (t Temperature) convertToKelvin() (float64, error) {
+	var k Temperature
 	switch t.Unit {
 	case "Celsius":
-		k = t.Value + 273.15
+		k.Value = t.Value + 273.15
 	case "Farenheit":
-		k = (t.Value-32)*5/9 + 273.5
-	case "Rakine":
-		k = t.Value * 5 / 9
+		k.Value = (t.Value-32)*5/9 + 273.5
+	case "Rakine", "rankine":
+		k.Value = t.Value * 5 / 9
 	default:
-		k = t.Value
+		k.Value = t.Value
 	}
-	if k < 0 {
+	if k.Value < 0 {
 		return 0, errors.New("temperature can't go below absolute zero")
 	}
-	return k, nil
-	// if t.Unit != "Kevin" {
-	// 	k = t.Value + 273.15
-	// 	if k < 0 {
-	// 		return 0, errors.New("temperature can't go below absolute zero")
-	// 	}
-	// 	return k, nil
-	// }
-	// return t.Value, nil
+	return k.Value, nil
+}
+func (t Temperature) convertToCelsius() (float64, error) {
+	var c float64
+	switch t.Unit {
+	case "Kelvin":
+		c = t.Value - 273.15
+	default:
+		c = t.Value
+	}
+	if c < -273.15 {
+		return 0, errors.New("temperature can't go below absolute zero")
+	}
+	return c, nil
+}
+func (t Temperature) convertToFarenheit() (float64, error) {}
+func (t Temperature) convertToRakine() (float64, error) {
+
 }
 
 func main() {
-	var boiling = Temperature{Value: 100, Unit: "Celsius", Symbol: "C"}
+	var boiling = Temperature{Value: 100, Unit: "Rakine", Symbol: "C"}
 	fmt.Println(boiling)
-	fmt.Println(boiling.convert())
+	fmt.Println(boiling.convertToKelvin())
 	fmt.Println(description)
 }
