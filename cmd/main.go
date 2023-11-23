fff5aeab3fc5e773344060d16b65dedc04635a74
Oluwadamilare <joeydre69@gmail.com>
2023-11-23 09:37:09 +0100

    initial command line implementation

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("no args")
+	} else {
+		for _, x := range os.Args[1:] {
+			fmt.Println(x)
+		}
+	}
+}
