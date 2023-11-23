b2042f8f3ce1a89ac24df4a663717426aa4b38b7
Oluwadamilare <joeydre69@gmail.com>
2023-11-23 12:46:37 +0100

    handling kelvin conversion in cmd

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strconv"
+
+	"github.com/themilar/vert"
 )
 
+func convertSToT(v float64, u string) vert.Temperature {
+	switch u {
+	case "k":
+		return vert.Temperature{v, "Kelvin", "K"}
+	case "c":
+		return vert.Temperature{v, "Celsius", "C"}
+	case "f":
+		return vert.Temperature{v, "Farenheit", "F"}
+	default:
+		return vert.Temperature{}
+	}
+}
 func main() {
+	// unit := flag.String("unit", "kelvin", "set the unit to convert your temperatures into")
+	k := flag.Bool("k", true, "convert the temperatures provided to kelvin")
+	flag.Parse()
 	if len(os.Args) == 1 {
-		fmt.Println("no args")
+		fmt.Println("insufficient arguments")
 	} else {
-		for _, x := range os.Args[1:] {
-			fmt.Println(x)
+		if *k {
+			for _, x := range os.Args[2:] {
+				// x[len(x)-1] this returns the unicode
+				value, unit := x[:len(x)-1], x[len(x)-1:]
+				// vert.Description[unit].Value=value
+				if v, err := strconv.ParseFloat(value, 64); err == nil {
+					t, _ := convertSToT(v, unit).ToKelvin()
+					fmt.Println(value, unit, t.Value)
+				}
+			}
 		}
 	}
 }
